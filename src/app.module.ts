5a6706d3cc8ac28d277fb9a39904c588ec16d571
Kang Jinhee <20wlsgml@gmail.com>
2022-11-09 11:49:54 +0900

    feat: logging middleware 추가구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,6 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes({ path: '.*', method: RequestMethod.ALL });
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }
--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,9 +1,23 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
   use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl, ip } = req;
+    const userAgent = req.get('user-agent');
+
+    res.on('finish', () => {
+      const { statusCode, statusMessage } = res;
+      const contentLength = res.get('content-length');
+
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${statusMessage} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
     next();
   }
 }
