8b4fcc014d0f46d2f38bd929b48f826905e59f6c
Kang Jinhee <20wlsgml@gmail.com>
2022-11-30 20:05:39 +0900

    fix: login API 수정
    1. login API에서 jwt token을 응답하도록 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,6 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
-    // Verify: user 저장소에서 해당 유저가 있는지 확인
     const user = await this.usersService.findOne(email);
 
-    // Compare: 저장된 user의 암호화 입력된 암호가 같은지 확인
     const hased = await bcrypt.hash(password, 10);
     const isMatch = await bcrypt.compare(user.password, hased);
 
     if (!isMatch) null;
 
-    // TODO: password 제거
     return user;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user._id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
