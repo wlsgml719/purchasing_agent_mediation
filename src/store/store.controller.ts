e2c3656a0e280e23c96f4c091d886a105c2a6933
Kang Jinhee <20wlsgml@gmail.com>
2022-12-10 21:39:07 +0900

    feat: Auth JwtAuthGuard 구성
    1. 로그인시 jwt 발행
    2. 상점 생성시 jwt를 통한 인증과정 적용

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { CreateStoreDto } from './dto/create-store.dto';
 import { StoreService } from './store.service';
 
@@ -6,6 +8,7 @@ import { StoreService } from './store.service';
 export class StoreController {
   constructor(private storeService: StoreService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createStoreDto: CreateStoreDto) {
     return await this.storeService.create(createStoreDto);
